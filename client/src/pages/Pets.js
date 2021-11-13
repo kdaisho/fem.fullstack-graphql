e896d349f1fd2589ea3cf22f760a06f0668a4dc9
Daisho <daishokomiyama@gmail.com>
2021-11-13 14:07:16 -0500

    Initial commit

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,38 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const AllPets = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
 
-export default function Pets () {
+export default function Pets() {
   const [modal, setModal] = useState(false)
+  const { data, loading, error } = useQuery(AllPets)
 
+  console.log({ data, loading, error })
 
   const onSubmit = input => {
     setModal(false)
   }
-  
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <p>error!</p>
+  }
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
 
   return (
-    <div className="page pets-page">
+    <div className='page pets-page'>
       <section>
-        <div className="row betwee-xs middle-xs">
-          <div className="col-xs-10">
+        <div className='row betwee-xs middle-xs'>
+          <div className='col-xs-10'>
             <h1>Pets</h1>
           </div>
 
-          <div className="col-xs-2">
+          <div className='col-xs-2'>
             <button onClick={() => setModal(true)}>new pet</button>
           </div>
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
